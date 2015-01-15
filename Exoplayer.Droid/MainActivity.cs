40da74d1bb7ae87f0a7c30da2f5666cf937e22cc
Marrtijn van Dijk <mhvdijk@gmail.com>
2015-01-15 17:02:22 +0100

    Updated sample code

--- Exoplayer.Droid/MainActivity.cs
+++ Exoplayer.Droid/MainActivity.cs
@@ -14,8 +14,7 @@ namespace Exoplayer.Droid
 	[Activity (Label = "Exoplayer.Droid", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
+		protected Com.Google.Android.Exoplayer.IExoPlayer mediaPlayer; 
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -29,6 +28,18 @@ namespace Exoplayer.Droid
 			Button button = FindViewById<Button> (Resource.Id.myButton);
 			
 			button.Click += delegate {
+				if (mediaPlayer == null) 
+				{ 
+					mediaPlayer = Com.Google.Android.Exoplayer.ExoPlayerFactory.NewInstance(1);
+					//mediaPlayer.AddListener(this); 
+				} 
+				Android.Net.Uri soundString = Android.Net.Uri.Parse("http://www.montemagno.com/sample.mp3");
+
+				FrameworkSampleSource sampleSource = new FrameworkSampleSource(this, soundString, null, 1); 
+				TrackRenderer aRenderer = new MediaCodecAudioTrackRenderer(sampleSource, null, true); 
+				mediaPlayer.Prepare(aRenderer);
+				mediaPlayer.PlayWhenReady = true;
+
 
 				//ExoPlayerFactory exo = ExoPlayerFactory.NewInstance(1);
 
@@ -37,18 +48,7 @@ namespace Exoplayer.Droid
 
 				Android.Net.Uri builtUri = Android.Net.Uri.Parse(BASE_URL);
 
-				// Build the sample source
-				FrameworkSampleSource sampleSource = new FrameworkSampleSource(Application.Context, builtUri, null, 1);
-
-				// Build the track renderers
-				TrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource, null, true);
-
-				// Build the ExoPlayer and start playback
-				var exo = ExoPlayerFactory.NewInstance(1);
-				exo.Prepare(audioRenderer);
-				exo.PlayWhenReady = true;
-
-				button.Text = string.Format ("{0} clicks!", count++);
+				button.Text = string.Format ("Status: {0}", mediaPlayer.PlaybackState);
 			};
 		}
 	}
