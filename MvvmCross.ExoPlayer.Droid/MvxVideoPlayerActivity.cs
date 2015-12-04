4556ed1d229a890c2877e09a919316469842cd93
mk <mk@infomantis.de>
2015-12-04 16:24:54 +0100

    Added MvvmCross component to wrap the ExoPlayer+
    The Activity and Player is based on the ExoPlayer Demo application

---
+++ MvvmCross.ExoPlayer.Droid/MvxVideoPlayerActivity.cs
@@ -0,0 +1,440 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.OS;
+using Android.Views;
+using Android.Views.Accessibility;
+using Android.Widget;
+using Cirrious.MvvmCross.Binding.BindingContext;
+using Cirrious.MvvmCross.Droid.Views;
+using Com.Google.Android.Exoplayer;
+using Com.Google.Android.Exoplayer.Audio;
+using Com.Google.Android.Exoplayer.Drm;
+using Com.Google.Android.Exoplayer.Text;
+using Com.Google.Android.Exoplayer.Util;
+using Java.Lang;
+using Java.Net;
+using MvvmCross.ExoPlayer.Droid.Player;
+using MvvmCross.ExoPlayer.Models;
+using MvvmCross.ExoPlayer.ViewModels;
+using Exception = Java.Lang.Exception;
+using Uri = Android.Net.Uri;
+
+namespace MvvmCross.ExoPlayer.Droid
+{
+	/// <summary>
+	/// An activity that plays media using <see cref="MvxVideoPlayer"/>.
+	/// </summary>
+	[Activity(
+		Name = "mvvmcross.exoplayer.droid.MvxVideoPlayerActivity",
+		ConfigurationChanges = ConfigChanges.KeyboardHidden | ConfigChanges.Keyboard | ConfigChanges.Orientation | ConfigChanges.ScreenSize,
+		LaunchMode = LaunchMode.SingleInstance
+		)]
+	public class MvxVideoPlayerActivity : MvxActivity<MvxVideoPlayerViewModel>,
+		ISurfaceHolderCallback,
+		MvxVideoPlayer.IListener,
+		MvxVideoPlayer.ICaptionListener,
+		MvxVideoPlayer.ID3MetadataListener,
+		AudioCapabilitiesReceiver.IListener
+	{
+		private static readonly CookieManager DefaultCookieManager;
+
+		static MvxVideoPlayerActivity()
+		{
+			DefaultCookieManager = new CookieManager();
+			DefaultCookieManager.SetCookiePolicy(CookiePolicy.AcceptOriginalServer);
+		}
+
+		private MvxVideoPlayerEventLogger _eventLogger;
+		private MediaController _mediaController;
+		private View _shutterView;
+		private AspectRatioFrameLayout _videoFrame;
+		private SurfaceView _surfaceView;
+		private SubtitleLayout _subtitleLayout;
+		private ProgressBar _progress;
+
+		private MvxVideoPlayer _player;
+		private bool _playerNeedsPrepare;
+		private long _playerPosition;
+
+		private MvxVideoItem _item;
+
+		private AudioCapabilitiesReceiver _audioCapabilitiesReceiver;
+		
+		public MvxVideoItem Item
+		{
+			get { return _item; }
+			set
+			{
+				if (_item == value)
+				{
+					return;
+				}
+
+				_item = value;
+				PreparePlayer(true);
+			}
+		}
+
+		public bool LoadingItem
+		{
+			get { return ViewModel.LoadingItem; }
+			set { _progress.Visibility = value ? ViewStates.Visible : ViewStates.Gone; }
+		}
+
+		#region Activity lifecycle
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.activity_videoplayer);
+			var root = FindViewById(Resource.Id.root);
+
+			root.Touch += (sender, args) =>
+			{
+				var motionEvent = args.Event;
+				switch (motionEvent.Action)
+				{
+					case MotionEventActions.Down:
+						ToggleControlsVisibility();
+						break;
+					case MotionEventActions.Up:
+						((View) sender).PerformClick();
+						break;
+				}
+				args.Handled = true;
+			};
+			root.KeyPress += (sender, args) =>
+			{
+				var keyCode = args.KeyCode;
+				if (keyCode == Keycode.Back || keyCode == Keycode.Escape
+				    || keyCode == Keycode.Menu)
+				{
+					args.Handled = false;
+				}
+				else
+				{
+					_mediaController.DispatchKeyEvent(args.Event);
+				}
+			};
+
+			_shutterView = FindViewById(Resource.Id.shutter);
+
+			_videoFrame = FindViewById<AspectRatioFrameLayout>(Resource.Id.video_frame);
+			_surfaceView = FindViewById<SurfaceView>(Resource.Id.surface_view);
+			_surfaceView.Holder.AddCallback(this);
+			_subtitleLayout = FindViewById<SubtitleLayout>(Resource.Id.subtitles);
+			_progress = FindViewById<ProgressBar>(Resource.Id.progress);
+
+			_mediaController = new MediaController(this);
+			_mediaController.SetAnchorView(root);
+
+			var currentHandler = CookieHandler.Default;
+			if (currentHandler != DefaultCookieManager)
+			{
+				CookieHandler.Default = DefaultCookieManager;
+			}
+
+			_audioCapabilitiesReceiver = new AudioCapabilitiesReceiver(this, this);
+			_audioCapabilitiesReceiver.Register();
+		}
+
+		protected override void OnNewIntent(Intent intent)
+		{
+			ReleasePlayer();
+			_playerPosition = 0;
+			Intent = intent;
+		}
+
+		protected override void OnViewModelSet()
+		{
+			base.OnViewModelSet();
+
+			var set = this.CreateBindingSet<MvxVideoPlayerActivity, MvxVideoPlayerViewModel>();
+			set.Bind(this).For(t => t.Item).To(vm => vm.VideoItem);
+			set.Bind(this).For(t => t.LoadingItem).To(vm => vm.LoadingItem);
+			set.Apply();
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+
+			ConfigureSubtitleView();
+			if (_player == null)
+			{
+				if (Item != null && Item.Url != null)
+				{
+					PreparePlayer(true);
+				}
+			}
+			else
+			{
+				_player.Backgrounded = false;
+			}
+		}
+
+		protected override void OnPause()
+		{
+			base.OnPause();
+			ReleasePlayer();
+			_shutterView.Visibility = ViewStates.Visible;
+		}
+
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+			_audioCapabilitiesReceiver.Unregister();
+			ReleasePlayer();
+		}
+
+		#endregion
+
+		#region AudioCapabilitiesReceiver.Listener methods
+
+		public void OnAudioCapabilitiesChanged(AudioCapabilities audioCapabilities)
+		{
+			if (_player == null)
+			{
+				return;
+			}
+			var backgrounded = _player.Backgrounded;
+			var playWhenReady = _player.PlayWhenReady;
+			ReleasePlayer();
+			PreparePlayer(playWhenReady);
+			_player.Backgrounded = backgrounded;
+		}
+
+		#endregion
+
+		#region Internal methods
+
+		private MvxVideoPlayer.IRendererBuilder GetRendererBuilder()
+		{
+			var userAgent = Util.GetUserAgent(this, "ExoPlayerDemo");
+			var url = Item.Url;
+			switch (Item.Type)
+			{
+				case MvxVideoItem.ContentType.Hls:
+					return new MvxHlsRendererBuilder(this, userAgent, url);
+				case MvxVideoItem.ContentType.Other:
+					return new MvxExtractorRendererBuilder(this, userAgent, Uri.Parse(url));
+				default:
+					throw new IllegalStateException("Unsupported type: " + Item.Type);
+			}
+		}
+
+		private void PreparePlayer(bool playWhenReady)
+		{
+			if (_player == null)
+			{
+				_player = new MvxVideoPlayer(GetRendererBuilder());
+				_player.AddListener(this);
+				_player.SetCaptionListener(this);
+				_player.SetMetadataListener(this);
+				_player.SeekTo(_playerPosition);
+				_playerNeedsPrepare = true;
+				_mediaController.SetMediaPlayer(_player.PlayerControl);
+				_mediaController.Enabled = true;
+				_eventLogger = new MvxVideoPlayerEventLogger();
+				_eventLogger.StartSession();
+				_player.AddListener(_eventLogger);
+				_player.SetInfoListener(_eventLogger);
+				_player.SetInternalErrorListener(_eventLogger);
+			}
+			if (_playerNeedsPrepare)
+			{
+				_player.Prepare();
+				_playerNeedsPrepare = false;
+			}
+			_player.Surface = _surfaceView.Holder.Surface;
+			_player.PlayWhenReady = playWhenReady;
+		}
+
+		private void ReleasePlayer()
+		{
+			if (_player == null)
+			{
+				return;
+			}
+
+			_playerPosition = _player.CurrentPosition;
+			_player.Release();
+			_player = null;
+			_eventLogger.EndSession();
+			_eventLogger = null;
+		}
+
+		#endregion
+
+		#region DemoPlayer.Listener implementation
+
+		public void OnStateChanged(bool playWhenReady, int playbackState)
+		{
+			if (playbackState == Com.Google.Android.Exoplayer.ExoPlayer.StateEnded)
+			{
+				ShowControls();
+			}
+		}
+
+		public void OnError(Exception e)
+		{
+			var exception = e as UnsupportedDrmException;
+			if (exception != null)
+			{
+				// TODO
+				// Special case DRM failures.
+				var msg = Util.SdkInt < 18
+					? "drm_error_not_supported"
+					: exception.Reason == UnsupportedDrmException.ReasonUnsupportedScheme
+						? "drm_error_unsupported_scheme"
+						: "drm_error_unknown";
+				Toast.MakeText(ApplicationContext, msg, ToastLength.Long).Show();
+			}
+			_playerNeedsPrepare = true;
+			ShowControls();
+		}
+
+		public void OnVideoSizeChanged(
+			int width,
+			int height,
+			int unappliedRotationDegrees,
+			float pixelWidthAspectRatio)
+		{
+			_shutterView.Visibility = ViewStates.Gone;
+			_videoFrame.SetAspectRatio(height == 0 ? 1 : (width*pixelWidthAspectRatio)/height);
+		}
+
+		#endregion
+
+		#region User controls
+
+		private void ToggleControlsVisibility()
+		{
+			if (_mediaController.IsShowing)
+			{
+				_mediaController.Hide();
+			}
+			else
+			{
+				ShowControls();
+			}
+		}
+
+		private void ShowControls()
+		{
+			_mediaController.Show(0);
+		}
+
+		#endregion
+
+		#region DemoPlayer.CaptionListener implementation
+
+		public void OnCues(IList<Cue> cues)
+		{
+			_subtitleLayout.SetCues(cues);
+		}
+
+		#endregion
+
+		#region DemoPlayer.MetadataListener implementation
+
+		public void OnId3Metadata(object metadata)
+		{
+			/*for (Map.Entry<String, Object> entry : metadata.entrySet()) {
+      if (TxxxMetadata.TYPE.equals(entry.getKey())) {
+        TxxxMetadata txxxMetadata = (TxxxMetadata) entry.getValue();
+        Log.i(TAG, String.format("ID3 TimedMetadata %s: description=%s, value=%s",
+            TxxxMetadata.TYPE, txxxMetadata.description, txxxMetadata.value));
+      } else if (PrivMetadata.TYPE.equals(entry.getKey())) {
+        PrivMetadata privMetadata = (PrivMetadata) entry.getValue();
+        Log.i(TAG, String.format("ID3 TimedMetadata %s: owner=%s",
+            PrivMetadata.TYPE, privMetadata.owner));
+      } else if (GeobMetadata.TYPE.equals(entry.getKey())) {
+        GeobMetadata geobMetadata = (GeobMetadata) entry.getValue();
+        Log.i(TAG, String.format("ID3 TimedMetadata %s: mimeType=%s, filename=%s, description=%s",
+            GeobMetadata.TYPE, geobMetadata.mimeType, geobMetadata.filename,
+            geobMetadata.description));
+      } else {
+        Log.i(TAG, String.format("ID3 TimedMetadata %s", entry.getKey()));
+      }
+    }*/
+		}
+
+		#endregion
+
+		#region SurfaceHolder.Callback implementation
+
+		public void SurfaceCreated(ISurfaceHolder holder)
+		{
+			if (_player != null)
+			{
+				_player.Surface = holder.Surface;
+			}
+		}
+
+		public void SurfaceChanged(ISurfaceHolder holder, Format format, int width, int height)
+		{
+			// Do nothing.
+		}
+
+		public void SurfaceDestroyed(ISurfaceHolder holder)
+		{
+			if (_player != null)
+			{
+				_player.BlockingClearSurface();
+			}
+		}
+
+		#endregion
+
+		private void ConfigureSubtitleView()
+		{
+			CaptionStyleCompat style;
+			float fontScale;
+			if (Util.SdkInt >= 19)
+			{
+				style = GetUserCaptionStyleV19();
+				fontScale = GetUserCaptionFontScaleV19();
+			}
+			else
+			{
+				style = CaptionStyleCompat.Default;
+				fontScale = 1.0f;
+			}
+			_subtitleLayout.SetStyle(style);
+			_subtitleLayout.SetFractionalTextSize(SubtitleLayout.DefaultTextSizeFraction*fontScale);
+		}
+
+		private float GetUserCaptionFontScaleV19()
+		{
+			var captioningManager = (CaptioningManager) GetSystemService(CaptioningService);
+			return captioningManager.FontScale;
+		}
+
+		private CaptionStyleCompat GetUserCaptionStyleV19()
+		{
+			var captioningManager = (CaptioningManager) GetSystemService(CaptioningService);
+			return CaptionStyleCompat.CreateFromCaptionStyle(captioningManager.UserStyle);
+		}
+	}
+}
---
+++ MvvmCross.ExoPlayer.Droid/Player/MvxVideoPlayerEventLogger.cs
@@ -0,0 +1,222 @@
+using Android.Media;
+using Android.Util;
+using Com.Google.Android.Exoplayer;
+using Com.Google.Android.Exoplayer.Chunk;
+using Com.Google.Android.Exoplayer.Util;
+using Java.IO;
+using Java.Lang;
+using Java.Text;
+using Java.Util;
+using AudioTrack = Com.Google.Android.Exoplayer.Audio.AudioTrack;
+using SystemClock = Android.OS.SystemClock;
+
+namespace MvvmCross.ExoPlayer.Droid.Player
+{
+	/// <summary>
+	/// Logs player events using {@link Log}.
+	/// </summary>
+	public class MvxVideoPlayerEventLogger : MvxVideoPlayer.IListener, MvxVideoPlayer.IInfoListener,
+		MvxVideoPlayer.IInternalErrorListener
+	{
+		private const string Tag = "EventLogger";
+
+		private static readonly NumberFormat TimeFormat;
+
+		static MvxVideoPlayerEventLogger()
+		{
+			TimeFormat = NumberFormat.GetInstance(Locale.Us);
+			TimeFormat.MinimumFractionDigits = 2;
+			TimeFormat.MaximumFractionDigits = 2;
+		}
+
+		private long _sessionStartTimeMs;
+		private readonly long[] _loadStartTimeMs;
+		private long[] _availableRangeValuesUs;
+
+		public MvxVideoPlayerEventLogger()
+		{
+			_loadStartTimeMs = new long[MvxVideoPlayer.RendererCount];
+		}
+
+		public void StartSession()
+		{
+			_sessionStartTimeMs = SystemClock.ElapsedRealtime();
+			Log.Debug(Tag, "start [0]");
+		}
+
+		public void EndSession()
+		{
+			Log.Debug(Tag, "end [" + GetSessionTimeString() + "]");
+		}
+
+		// DemoPlayer.Listener
+
+		public void OnStateChanged(bool playWhenReady, int state)
+		{
+			Log.Debug(Tag, "state [" + GetSessionTimeString() + ", " + playWhenReady + ", "
+			               + GetStateString(state) + "]");
+		}
+
+		public void OnError(Exception e)
+		{
+			Log.Error(Tag, "playerFailed [" + GetSessionTimeString() + "]", e);
+		}
+
+		public void OnVideoSizeChanged(
+			int width,
+			int height,
+			int unappliedRotationDegrees,
+			float pixelWidthHeightRatio)
+		{
+			Log.Debug(Tag, "videoSizeChanged [" + width + ", " + height + ", " + unappliedRotationDegrees
+			               + ", " + pixelWidthHeightRatio + "]");
+		}
+
+		// DemoPlayer.InfoListener
+
+		public void OnBandwidthSample(int elapsedMs, long bytes, long bitrateEstimate)
+		{
+			Log.Debug(Tag, "bandwidth [" + GetSessionTimeString() + ", " + bytes + ", "
+			               + GetTimeString(elapsedMs) + ", " + bitrateEstimate + "]");
+		}
+
+		public void OnDroppedFrames(int count, long elapsed)
+		{
+			Log.Debug(Tag, "droppedFrames [" + GetSessionTimeString() + ", " + count + "]");
+		}
+
+		public void OnLoadStarted(
+			int sourceId,
+			long length,
+			int type,
+			int trigger,
+			Format format,
+			long mediaStartTimeMs,
+			long mediaEndTimeMs)
+		{
+			_loadStartTimeMs[sourceId] = SystemClock.ElapsedRealtime();
+			if (VerboseLogUtil.IsTagEnabled(Tag))
+			{
+				Log.Verbose(Tag, "loadStart [" + GetSessionTimeString() + ", " + sourceId + ", " + type
+				                 + ", " + mediaStartTimeMs + ", " + mediaEndTimeMs + "]");
+			}
+		}
+
+		public void OnLoadCompleted(
+			int sourceId,
+			long bytesLoaded,
+			int type,
+			int trigger,
+			Format format,
+			long mediaStartTimeMs,
+			long mediaEndTimeMs,
+			long elapsedRealtimeMs,
+			long loadDurationMs)
+		{
+			if (VerboseLogUtil.IsTagEnabled(Tag))
+			{
+				long downloadTime = SystemClock.ElapsedRealtime() - _loadStartTimeMs[sourceId];
+				Log.Verbose(Tag, "loadEnd [" + GetSessionTimeString() + ", " + sourceId + ", " + downloadTime
+				                 + "]");
+			}
+		}
+
+		public void OnVideoFormatEnabled(Format format, int trigger, long mediaTimeMs)
+		{
+			Log.Debug(Tag, "videoFormat [" + GetSessionTimeString() + ", " + format.Id + ", "
+			               + trigger.ToString() + "]");
+		}
+
+		public void OnAudioFormatEnabled(Format format, int trigger, long mediaTimeMs)
+		{
+			Log.Debug(Tag, "audioFormat [" + GetSessionTimeString() + ", " + format.Id + ", "
+			               + trigger.ToString() + "]");
+		}
+
+		// DemoPlayer.InternalErrorListener
+
+		public void OnLoadError(int sourceId, IOException e)
+		{
+			PrintInternalError("loadError", e);
+		}
+
+		public void OnRendererInitializationError(Exception e)
+		{
+			PrintInternalError("rendererInitError", e);
+		}
+
+		public void OnDrmSessionManagerError(Exception e)
+		{
+			PrintInternalError("drmSessionManagerError", e);
+		}
+
+		public void OnDecoderInitializationError(MediaCodecTrackRenderer.DecoderInitializationException e)
+		{
+			PrintInternalError("decoderInitializationError", e);
+		}
+
+		public void OnAudioTrackInitializationError(AudioTrack.InitializationException e)
+		{
+			PrintInternalError("audioTrackInitializationError", e);
+		}
+
+		public void OnAudioTrackWriteError(AudioTrack.WriteException e)
+		{
+			PrintInternalError("audioTrackWriteError", e);
+		}
+
+		public void OnCryptoError(MediaCodec.CryptoException e)
+		{
+			PrintInternalError("cryptoError", e);
+		}
+
+		public void OnDecoderInitialized(
+			string decoderName,
+			long elapsedRealtimeMs,
+			long initializationDurationMs)
+		{
+			Log.Debug(Tag, "decoderInitialized [" + GetSessionTimeString() + ", " + decoderName + "]");
+		}
+
+		public void OnAvailableRangeChanged(ITimeRange availableRange)
+		{
+			_availableRangeValuesUs = availableRange.GetCurrentBoundsUs(_availableRangeValuesUs);
+			Log.Debug(Tag, "availableRange [" + availableRange.IsStatic + ", " + _availableRangeValuesUs[0]
+			               + ", " + _availableRangeValuesUs[1] + "]");
+		}
+
+		private void PrintInternalError(string type, Exception e)
+		{
+			Log.Error(Tag, "internalError [" + GetSessionTimeString() + ", " + type + "]", e);
+		}
+
+		private string GetStateString(int state)
+		{
+			switch (state)
+			{
+				case Com.Google.Android.Exoplayer.ExoPlayer.StateBuffering:
+					return "B";
+				case Com.Google.Android.Exoplayer.ExoPlayer.StateEnded:
+					return "E";
+				case Com.Google.Android.Exoplayer.ExoPlayer.StateIdle:
+					return "I";
+				case Com.Google.Android.Exoplayer.ExoPlayer.StatePreparing:
+					return "P";
+				case Com.Google.Android.Exoplayer.ExoPlayer.StateReady:
+					return "R";
+				default:
+					return "?";
+			}
+		}
+
+		private string GetSessionTimeString()
+		{
+			return GetTimeString(SystemClock.ElapsedRealtime() - _sessionStartTimeMs);
+		}
+
+		private string GetTimeString(long timeMs)
+		{
+			return TimeFormat.Format((timeMs)/1000f);
+		}
+	}
+}
