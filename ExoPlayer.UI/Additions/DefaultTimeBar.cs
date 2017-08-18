5efbeec1305f67382f812b96b62d7f2a7d92511e
Martijn van Dijk <mhvdijk@gmail.com>
2017-08-18 14:42:47 +0200

    Update ExoPlayer to 2.5.1

---
+++ ExoPlayer.UI/Additions/DefaultTimeBar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Com.Google.Android.Exoplayer2.UI
+{
+    public partial class DefaultTimeBar
+    {
+        public void SetEnabled(bool enabled)
+        {
+            throw new NotImplementedException("Not available in the bindings currently");
+        }
+    }
+}
