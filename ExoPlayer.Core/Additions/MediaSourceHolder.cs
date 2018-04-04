1c8d8ac5abf1d922ebaa9de8ad8efb9548e75a31
Martijn van Dijk <mhvdijk@gmail.com>
2018-04-04 21:10:06 +0200

    Fix some bindings

---
+++ ExoPlayer.Core/Additions/MediaSourceHolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Java.Lang;
+
+namespace Com.Google.Android.Exoplayer2.Source
+{
+    public sealed partial class DynamicConcatenatingMediaSource
+    {
+        public sealed partial class MediaSourceHolder : global::Java.Lang.Object, global::Java.Lang.IComparable
+        {
+            public int CompareTo(Java.Lang.Object o)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
