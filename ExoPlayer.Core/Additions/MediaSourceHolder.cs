a9f0d6988b280087c51bcff63580ba4a42828725
Martijn van Dijk <mhvdijk@gmail.com>
2018-05-09 18:58:26 +0200

    Update to 2.8.0

--- ExoPlayer.Core/Additions/MediaSourceHolder.cs
+++ ExoPlayer.Core/Additions/MediaSourceHolder.cs
@@ -13,4 +13,15 @@ namespace Com.Google.Android.Exoplayer2.Source
             }
         }
     }
+
+	public partial class ConcatenatingMediaSource
+	{
+		public sealed partial class MediaSourceHolder : global::Java.Lang.Object, global::Java.Lang.IComparable 
+		{
+			public int CompareTo(Java.Lang.Object o)
+            {
+                throw new NotImplementedException();
+            }
+		}
+	}
 }
--- ExoPlayer.SmoothStreaming/Additions/TrackKey.cs
+++ ExoPlayer.SmoothStreaming/Additions/TrackKey.cs
@@ -2,11 +2,11 @@ using Java.Lang;
 
 namespace Com.Google.Android.Exoplayer2.Source.Smoothstreaming.Manifest
 {
-    public partial class TrackKey
-    {
-        public int CompareTo(Object o)
-        {
-            return this.CompareTo(o as TrackKey);
-        }
-    }
+    //public partial class TrackKey
+    //{
+    //    public int CompareTo(Object o)
+    //    {
+    //        return this.CompareTo(o as TrackKey);
+    //    }
+    //}
 }
