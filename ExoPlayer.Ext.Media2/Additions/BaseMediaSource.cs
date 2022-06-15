8273e2d41ccfd59d485f82099726678f7b87dab6
Jan Wiebe de Haan <janwiebe@jump.nl>
2022-06-15 17:15:45 +0200

    try fix media2

--- ExoPlayer.Ext.Media2/Additions/BaseMediaSource.cs
+++ ExoPlayer.Ext.Media2/Additions/BaseMediaSource.cs
@@ -5,15 +5,15 @@ using System.Text;
 namespace Com.Google.Android.Exoplayer2.Source
 {
 
-    // Metadata.xml XPath class reference: path="/api/package[@name='com.google.android.exoplayer2.source']/class[@name='BaseMediaSource']"
-    public abstract partial class BaseMediaSource : global::Java.Lang.Object, global::Com.Google.Android.Exoplayer2.Source.IMediaSource
-    {
+    //// Metadata.xml XPath class reference: path="/api/package[@name='com.google.android.exoplayer2.source']/class[@name='BaseMediaSource']"
+    //public abstract partial class BaseMediaSource : global::Java.Lang.Object, global::Com.Google.Android.Exoplayer2.Source.IMediaSource
+    //{
 
-        //public abstract global::Java.Lang.Object Tag { get; }
-    }
+    //    //public abstract global::Java.Lang.Object Tag { get; }
+    //}
 
-    internal partial class BaseMediaSourceInvoker : BaseMediaSource
-    {
-        //public override Java.Lang.Object Tag => throw new NotImplementedException();
-    }
+    //internal partial class BaseMediaSourceInvoker : BaseMediaSource
+    //{
+    //    //public override Java.Lang.Object Tag => throw new NotImplementedException();
+    //}
 }
