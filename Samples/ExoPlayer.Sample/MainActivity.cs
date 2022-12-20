80f9d6bc0f2cfa56534378c6a0b7bd8445085fac
Martijn van Dijk <mhvdijk@gmail.com>
2022-12-20 12:32:07 +0100

    Add new sample code

---
+++ Samples/ExoPlayer.Sample/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.Content;
+
+namespace ExoPlayer.Sample;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+
+
+        var HttpDataSourceFactory = new DefaultHttpDataSource.Factory().SetAllowCrossProtocolRedirects(true);
+        var MainDataSource = new ProgressiveMediaSource.Factory(HttpDataSourceFactory);
+        var Exoplayer = new IExoPlayer.Builder(Context).SetMediaSourceFactory(MainDataSource).Build();
+
+        MediaItem mediaItem = MediaItem.FromUri(Android.Net.Uri.Parse("https://ia800806.us.archive.org/15/items/Mp3Playlist_555/AaronNeville-CrazyLove.mp3"));
+
+        Exoplayer.AddMediaItem(mediaItem);
+        Exoplayer.Prepare();
+        Exoplayer.PlayWhenReady = true;
+    }
+}
+
