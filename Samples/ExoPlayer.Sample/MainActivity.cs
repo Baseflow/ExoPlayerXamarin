66efdc75e8e0101519d973d49ecf177a332fe9ad
Martijn van Dijk <mhvdijk@gmail.com>
2023-01-04 20:17:23 +0100

    Fix sample

--- Samples/ExoPlayer.Sample/MainActivity.cs
+++ Samples/ExoPlayer.Sample/MainActivity.cs
@@ -1,4 +1,8 @@
 ﻿using Android.Content;
+using Com.Google.Android.Exoplayer2;
+using Com.Google.Android.Exoplayer2.Source;
+using Com.Google.Android.Exoplayer2.UI;
+using Com.Google.Android.Exoplayer2.Upstream;
 
 namespace ExoPlayer.Sample;
 
@@ -12,16 +16,20 @@ public class MainActivity : Activity
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
 
+        var exoPlayerView = FindViewById<StyledPlayerView>(Resource.Id.player_view_sample);
 
         var HttpDataSourceFactory = new DefaultHttpDataSource.Factory().SetAllowCrossProtocolRedirects(true);
         var MainDataSource = new ProgressiveMediaSource.Factory(HttpDataSourceFactory);
-        var Exoplayer = new IExoPlayer.Builder(Context).SetMediaSourceFactory(MainDataSource).Build();
+        var Exoplayer = new IExoPlayer.Builder(this.ApplicationContext).SetMediaSourceFactory(MainDataSource).Build();
 
-        MediaItem mediaItem = MediaItem.FromUri(Android.Net.Uri.Parse("https://ia800806.us.archive.org/15/items/Mp3Playlist_555/AaronNeville-CrazyLove.mp3"));
+        var mediaItem1 = MediaItem.FromUri(Android.Net.Uri.Parse("https://ia800806.us.archive.org/15/items/Mp3Playlist_555/AaronNeville-CrazyLove.mp3"));
+        var mediaItem2 = MediaItem.FromUri(Android.Net.Uri.Parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"));
 
-        Exoplayer.AddMediaItem(mediaItem);
-        Exoplayer.Prepare();
-        Exoplayer.PlayWhenReady = true;
+        exoPlayerView.Player = Exoplayer;
+        exoPlayerView.Player.AddMediaItem(mediaItem1);
+        exoPlayerView.Player.AddMediaItem(mediaItem2);
+        exoPlayerView.Player.Prepare();
+        exoPlayerView.Player.PlayWhenReady = true;
     }
 }
 
